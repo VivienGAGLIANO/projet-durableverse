5e8563f2879e37722f02c0dbe8f69550ad478239
Vivien Gagliano <vivien.gagliano@ensiie.fr>
2020-03-18 00:08:10 +0100

    updated documentation

--- card.h
+++ card.h
@@ -3,8 +3,14 @@
 
 
 /*---------------------------- Staff card -----------------------------------*/
-enum staff_effect_id {AE1, AE2, AA1, AA2, RE1, RE2, RA1, RA2, ADD, RDD, DR, E}; 
+/*
+ *@brief enumeration for staff card effects
+ */
+enum staff_effect_id {AE1, AE2, AA1, AA2, RE1, RE2, RA1, RA2, ASD, RSD, DR, E}; 
 
+/*
+ *@brief staff card struct : composed of the effect of the card, as well as its "effectiveness"
+ */
 struct staff_effect {
     enum staff_effect_id id;
     int value;
@@ -13,14 +19,17 @@ struct staff_effect {
 
 
 /*---------------------------- Action card ----------------------------------*/
-enum action_effect {WinOneDD, DrawOneCard, PlayOneFISE, PlayOneFISA, Win6EP, RemoveOneFISEFISA, DiscardOneStaff, ShuffleDiscardDraw, IncreaseDevelopment, IncreaseDurability, RemoveAllFISEFISA};
+/*
+ *@brief enumeration for action card effects
+ */
+enum action_effect {WinOneSD, DrawOneCard, PlayOneFISE, PlayOneFISA, Win6EP, RemoveOneFISEFISA, DiscardOneStaff, ShuffleDiscardDraw, IncreaseDevelopment, IncreaseDurability, RemoveAllFISEFISA};
 
 
 
 enum action_effect;
 
 /*
- *@brief general card type definition, for action and staff cards. Includes card's name, cost, and two pointers (one of which will be NULL) : if action card, points toward the effect of the card (as enumeration), else if staff card, points towards a couple (effect, value), representing the effect of the card, as well as its "effectiveness"
+ *@brief general card type definition, for action and staff cards. Includes card's name, cost, and two pointers (one of which will be NULL) poiting towards the effect of the card, being action or staff
  */
 struct card {
     char* name;
