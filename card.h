e499932d7ef170aa3a897127e8bce3279d6e4f22
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-03-17 23:08:15 +0100

    Fixed some mistakes

--- card.h
+++ card.h
@@ -1,3 +1,8 @@
+
+enum staff_effect_id {AE1, AE2, AA1, AA2, RE1, RE2, RA1, RA2, ADD, RDD, DR, E};
+
+enum action_effect {Win1DD, PickOneCard, Play1FISE, Play1FISA, Win6EP, RemoveFISEFISA};
+
 struct card {
     char* name;
     int cost;
@@ -6,10 +11,6 @@ struct card {
 };
 
 struct staff_effect {
+    enum staff_effect_id id;
     int value;
-    staff_effect_id id;
 };
-
-enum staff_effect_id {AE1, AE2, AA1, AA2, RE1, RE2, RA1, RA2, ADD, RDD, DR, E};
-
-enum action_effect {Win1DD, PickOneCard, Play1FISE, Play1FISA, Win6EP, RemoveFISEFISA};
