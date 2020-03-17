fc2140bc0c48db43945b3b705d604dff9104623c
Vivien Gagliano <vivien.gagliano@ensiie.fr>
2020-03-17 23:36:13 +0100

    updated card.h : completed enumeration for action card effect, and added doc

--- card.h
+++ card.h
@@ -1,8 +1,13 @@
+#ifndef _CARD_H
+#define _CARD_H
 
-enum staff_effect_id {AE1, AE2, AA1, AA2, RE1, RE2, RA1, RA2, ADD, RDD, DR, E};
+enum staff_effect_id; 
 
-enum action_effect {Win1DD, PickOneCard, Play1FISE, Play1FISA, Win6EP, RemoveFISEFISA};
+enum action_effect;
 
+/*
+ *@brief general card type definition, for action and staff cards. Includes card's name, cost, and two pointers (one of which will be NULL) : if action card, points toward the effect of the card (as enumeration), else if staff card, points towards a couple (effect, value), representing the effect of the card, as well as its "effectiveness"
+ */
 struct card {
     char* name;
     int cost;
@@ -10,7 +15,19 @@ struct card {
     enum action_effect* action_effect;
 };
 
+
+//---------------------------- Staff card -----------------------------------
 struct staff_effect {
     enum staff_effect_id id;
     int value;
 };
+
+enum staff_effect_id {AE1, AE2, AA1, AA2, RE1, RE2, RA1, RA2, ADD, RDD, DR, E};
+
+
+//---------------------------- Action card ----------------------------------
+enum action_effect {WinOneDD, DrawOneCard, PlayOneFISE, PlayOneFISA, Win6EP, RemoveOneFISEFISA, DiscardOneStaff, ShuffleDiscardDraw, IncreaseDevelopment, IncreaseDurability, RemoveAllFISEFISA};
+
+
+
+#endif
