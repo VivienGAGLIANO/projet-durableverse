c5f03114b0a289e8955bb39a4b66413c7040706e
Mathilda Rosset <matylda.rosset@gmail.com>
2020-03-18 11:39:36 +0100

    Updated the interface.h and its comments

---
+++ interface.h
@@ -0,0 +1,44 @@
+#ifndef _INTERFACE_H
+#define _INTERFACE_H
+
+#include "card.h"
+#include "board.h"
+
+/*
+*@brief check the turn number and if it's even or odd
+*@return the turn number and if FISA appear or disappear and print that a new one begin, print if there is a new staff space, SD of each players  
+*/
+void print_new_turn(struct board);
+
+/*
+*@brief check the phase and the player
+*@return the phase number, the player, print the board, and current player's hand
+*/
+void print_new_phase(struct board);
+
+/*
+* 
+*@return prints the current board except the hand and the discard
+*/
+void print_board(struct board);
+
+/*
+*@detail ask how many student cards (FISE or FISA) wanted: if one is wanted, then ask which one, if more, the ask how many. 
+Then check if the quantity wanted is available. The choice is savec in variant int FISE or int FISA
+*/
+void choice_FISE_FISA(int nb_FISE*, int nb_FISA*, struct ensiie);
+
+/*
+*@detail ask the player to play a hand card or to end the phase. Check if the cost < EP, if it's not then ask again with an error message.
+If all hand card cost < EP then the only choice is to end the phase
+@return prints EP, hand cards
+*/
+void choice_card(struct card*, struct ensiie*);
+
+/*
+*@return prints the winner or equality
+*/
+void print_end_game (struct board);
+
+@
+#endif
