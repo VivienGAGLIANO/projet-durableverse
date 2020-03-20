1b28a1d4ccd0f4a7c2b38d0d90ad816e943b7497
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-03-20 19:45:44 +0100

    Updated documentation

--- board.h
+++ board.h
@@ -6,10 +6,11 @@
 #define MAX_CARD_NUMBER 50
 
 /* -------------------------------------------------------- */
-/* -------------------- Defining types -------------------- */
+/* ------------------------- Types ------------------------ */
 /* -------------------------------------------------------- */
 
-/* General type for the staff cards currently on the board, and the maximum number of staff
+/**
+ * @brief General type for the staff cards currently on the board, and the maximum number of staff
  * cards allowed on the board
  */
 struct board_staff {
@@ -17,118 +18,122 @@ struct board_staff {
     int max;
 };
 
-/* General type for the students cards currently on the board (FISE and FISA),
+/**
+ * @brief General type for the students cards currently on the board (FISE and FISA),
  * the counter and the durability and development points for each type of student card
  */
 struct board_students {
-    int FISE_count;
-    int FISA_count;
-    int FISE_durability;
-    int FISA_durability;
-    int FISE_development;
-    int FISA_development;
+    int FISE_count; /** The number of FISE cards currenlty on the board */
+    int FISA_count; /** The number of FISA cards currenlty on the board */
+    int FISE_durability; /** Each FISE card's durabilty */
+    int FISA_durability; /** Each FISA card's durabilty */
+    int FISE_development; /** Each FISE card's development points */
+    int FISA_development; /** Each FISA card's development points */
 };
 
-/* General type for one player (one ENSIIE), with its Sustainable Development points (int SD),
- * its deck, hand, and discard (which are lists/array of struct card), the current staff cards
- * and student cards currently on the board, and its opponent
+/**
+ * @brief General type for one player (one ENSIIE), with its Sustainable Development points (int SD),
+ * its deck, hand, and discard (which are lists/arrays of struct card), the current staff cards
+ * and student cards currently on the board, and a pointer to its opponent
  */
 struct ensiie {
-    int SD;
-    struct card* deck;
-    struct card hand[MAX_CARD_NUMBER];
-    struct card* discard;
-    struct board_staff current_staff;
-    struct board_students current_students;
-    struct ensiie* opponent_ensiie;
+    int SD; /** The sustainable development points of the ensiie */
+    struct card* deck; /** The deck */
+    struct card hand[MAX_CARD_NUMBER]; /** The hand */
+    struct card* discard; /** The discard */
+    struct board_staff current_staff; /** The current staff cards currently on the board */
+    struct board_students current_students; /** The current student cards currently on the board */
+    struct ensiie* opponent; /** A pointer to the opponent ensiie */
 };
 
-/* General type for the game board, including the two players and the turn counter
+/**
+ * @brief General type for the game board, including the two players and the turn counter
  */
 struct board {
-    struct ensiie player1;
-    struct ensiie player2;
-    int n_turn;
+    struct ensiie player1; /** The first player */
+    struct ensiie player2; /** The second player */
+    int n_turn; /** The current turn number */
 };
 
 
 /* ------------------------------------------------------------ */
-/* -------------------- Defining functions -------------------- */
+/* ------------------------- Functions ------------------------ */
 /* ------------------------------------------------------------ */
 
-/* 
+/**
  * @brief creates new board and allocates memory
  * @return initialized board instance 
  */
 struct board new_board();
 
-/* 
+/**
  * @brief frees memory taken by given board, including memory taken for both ensiie struct
  * 
  */
 void free_board(struct board*);
 
-/* 
+/**
  * @brief updates board state at the beginning of a new turn
  * @detail increments turn counter, handles FISA student cards turn based behaviour, updates number of space available for staff cards
  */
 void begin_turn(struct board*);
 
-/* 
+/**
  * @brief computes number of cards drawn by player
  * @return number of cards to be drawn
  */
 int nb_card_drawn(struct ensiie);
 
-/* 
+/**
  * @brief draws a card for the given player
  * 
  */
 void draw(struct ensiie*);
 
-/* 
+/**
  * @brief computes number of student cards received by the player
  * @return number of student cards to be received
  */
 int nb_student_card_received(struct ensiie);
 
-/* 
+/**
  * @brief adds given amount of FISE student cards to the player
  * 
  */
 void add_student_FISE(int, struct ensiie*);
 
-/* 
+/**
  * @brief adds given amount of FISA student cards to the player
  * 
  */
 void add_student_FISA(int, struct ensiie*);
 
-/* 
+/**
  * @brief computes Energy Points (EP) available at the beginning of the player's phase
  * @return number of EP available
  */
 int available_EP(struct ensiie);
 
-/*
- *@brief start a player's phase
- *
+/**
+ * @brief starts a player's phase
+ * @param a pointer to the ensiie playing, and the turn number
  */
-void play_phase (struct ensiie*, int);
+void play_phase(struct ensiie*, int);
 
-/* 
+/**
  * @brief allows player to play one card from its hand
- * @return 1 if card played, 0 if turn passed
+ * @param the ensiie which is playing, and the EP 
+ * @return 1 if card played, 0 if turn passed by user
  */
-int play_card(struct ensiie*, int*);
+int play_card(struct ensiie*, int* ep);
 
-/* 
+/**
  * @brief ends current turn, computes and updates Sustainable Development (SD) points for each player
  * 
  */
 void end_turn(struct board*);
 
-/* 
+/**
  * @brief checks if game is over, i.e. if winning condition of having 20+ Sustainable Development (SD) points is achieved
  * @return 1 if game is over, 0 else
  */
--- card.h
+++ card.h
@@ -2,42 +2,38 @@
 #define _CARD_H
 
 
-/*---------------------------- Staff card -----------------------------------*/
-/*
- *@brief enumeration for staff card effects
+/* ------------------------ Staff card ------------------------ */
+/**
+ * @brief enumeration for staff card effects
  */
 enum staff_effect_id {AE1, AE2, AA1, AA2, RE1, RE2, RA1, RA2, ASD, RSD, DR, E}; 
 
-/*
- *@brief staff card struct : composed of the effect of the card, as well as its "effectiveness"
+/**
+ * @brief staff card struct: composed of the effect of the card, as well as its "effectiveness"
  */
 struct staff_effect {
-    enum staff_effect_id id;
-    int value;
+    enum staff_effect_id id; /** The effect ID of the card */
+    int value; /** The value applied to the effect */
 };
 
 
-
-/*---------------------------- Action card ----------------------------------*/
-/*
- *@brief enumeration for action card effects
+/* ------------------------ Action card ----------------------- */
+/**
+ * @brief enumeration for action card effects
  */
 enum action_effect {WinOneSD, DrawOneCard, PlayOneFISE, PlayOneFISA, Win6EP, RemoveOneFISEFISA, DiscardOneStaff, ShuffleDiscardDraw, IncreaseDevelopment, IncreaseDurability, RemoveAllFISEFISA};
 
 
-
-enum action_effect;
-
-/*
- *@brief general card type definition, for action and staff cards. Includes card's name, cost, and two pointers (one of which will be NULL) poiting towards the effect of the card, being action or staff
+/* ------------------------ General card ----------------------- */
+/**
+ * @brief general card type definition, for action and staff cards. Includes card's name, cost, and two pointers (one of which will be NULL) poiting towards the effect of the card, being action or staff
  */
 struct card {
-    char* name;
-    int cost;
-    struct staff_effect* staff_effect;
-    enum action_effect* action_effect;
+    char* name; /** The name of the card */
+    int cost; /** The EP cost of the card */
+    struct staff_effect* staff_effect; /** */
+    enum action_effect* action_effect; /** */
 };
-
 
 
 #endif
