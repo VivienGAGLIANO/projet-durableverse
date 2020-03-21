06c9224ceb4a09ae2eb05fe1ced2189adb528df8
unknown <yabar1@hotmail.fr>
2020-03-22 00:31:09 +0100

    tout structure sauf la derniere procédure

--- src/structure.c
+++ src/structure.c
@@ -23,3 +23,38 @@ int pop_stack(stack* a){
 	return a->val;
 }
 
+void push_last_stack(int e, stack* a){
+	stack * p;
+	while a->next!=NULL{
+		a->next;
+	}
+	p->val=e;
+	p->next=NULL;
+	a->next=p;
+}
+int pop_last_stack(stack* a){
+	while (a->next)->next!=NULL{
+		*a=a->next;
+
+	}
+	p=(a->next)->val;
+	a->next=NULL;
+	return a->val;
+}
+int get_stack_ele_position(elm e, stack a){
+	int i = 0;
+	while a->val!=e{
+		a=a->next;
+		i=i+1;
+	}
+	return i;
+}
+
+int len_stack(stack a){
+	int i = 0;
+	while a->next!=NULL{
+		a=a->next;
+		i=i+1;
+	}
+	return i;
+}
