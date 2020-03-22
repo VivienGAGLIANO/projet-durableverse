f7d4e3794f825d2663f78d83bdecd95d13312315
unknown <yabar1@hotmail.fr>
2020-03-22 20:28:46 +0100

    ajout de play_phase

--- headers/board.h
+++ headers/board.h
@@ -67,11 +67,18 @@ struct board new_board();
  */
 void free_board(struct board*);
 
+
 /* 
  * @brief updates board state at the beginning of a new turn
  * @detail increments turn counter, handles FISA student cards turn based behaviour, updates number of space available for staff cards
  */
 void begin_turn(struct board*);
+
+/*
+ *@brief allows the ensiie to play his phase
+ *@detail calculate the EP of the ensiie, what FISA OR FISE card the ensiie wants to play, what action and staff card the ensiie wants to play, apply the effect of these cards on the other ensiie 
+ */
+void play_phase(ensiie* , int);
 
 /* 
  * @brief computes number of cards drawn by player
@@ -121,11 +128,37 @@ void play_card(struct ensiie*);
  */
 void end_turn(struct board*);
 
-/* 
+/*
  * @brief checks if game is over, i.e. if winning condition of having 20+ Sustainable Development (SD) points is achieved
  * @return 1 if game is over, 0 else
  */
 int is_over(struct board);
 
+/*
+ * @brief give the ensiie's deck
+ * @return a pointeur towards the ensiie deck
+ */
 
+struct card* get_ensiie_deck(struct ensiie);
+
+/*
+ * @brief give the ensiie's deck
+ * @return a table which contain all the card in the ensiie's hand
+ */
+
+struct card get_ensiie_hand(struct ensiie);
+
+/*
+ * @brief give the ensiie's deck
+ * @return a pointeur towards the ensiie's discard
+ */
+
+struct card* get_ensiie_discard(struct ensiie);
+
+/*
+ * @brief tell if the tour is peer or not 
+ * @return 1 if the tour is peer, else 0
+ */
+
+int is_peer(struct board);
 #endif
