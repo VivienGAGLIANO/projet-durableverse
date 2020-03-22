5c002aed49c5d34989a300f31e6207f094abc3cb
unknown <yabar1@hotmail.fr>
2020-03-22 19:10:26 +0100

    corrections

--- src/structure.c
+++ src/structure.c
@@ -1,18 +1,19 @@
 #include "structure.h"
 
 stack create_stack(){
-	a=malloc(sizeof(stack));
+	int a=malloc(sizeof(stack));
 	return a;
 }
 
 int is_empty_stack(stack a){
-	if a==NULL
+	if (a==NULL)
 		return 1;
 	else
 		return 0;
 }
 
 void push_stack(int e, stack* a){
+	stack p;
 	p=*a;
 	a->val=e;
 	a->next=p;
@@ -25,7 +26,7 @@ int pop_stack(stack* a){
 
 void push_last_stack(int e, stack* a){
 	stack * p;
-	while a->next!=NULL{
+	while (a->next!=NULL){
 		a->next;
 	}
 	p->val=e;
@@ -33,17 +34,18 @@ void push_last_stack(int e, stack* a){
 	a->next=p;
 }
 int pop_last_stack(stack* a){
-	while (a->next)->next!=NULL{
+	while ((a->next)->next!=NULL){
 		*a=a->next;
 
 	}
+	int p;
 	p=(a->next)->val;
 	a->next=NULL;
 	return a->val;
 }
 int get_stack_ele_position(elm e, stack a){
 	int i = 0;
-	while a->val!=e{
+	while (a->val!=e){
 		a=a->next;
 		i=i+1;
 	}
@@ -52,7 +54,7 @@ int get_stack_ele_position(elm e, stack a){
 
 int len_stack(stack a){
 	int i = 0;
-	while a->next!=NULL{
+	while (a->next!=NULL){
 		a=a->next;
 		i=i+1;
 	}
