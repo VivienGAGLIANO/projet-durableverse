1a637b7565ba639718662488efe9224837407abb
unknown <yabar1@hotmail.fr>
2020-04-03 16:42:23 +0200

    Corrections du board.c

--- src/board.c
+++ src/board.c
@@ -1,9 +1,9 @@
 #include "board.h"
 
 
-struct board new board(){
+struct board new_board(){
 	struct board board;
-	&board=malloc(sizeof(struct board));
+	// board=malloc(sizeof(struct board));
 	return board;
 }
 
@@ -11,9 +11,9 @@ void free_board(struct board* b){
 	free(b);
 }
 
-void begin turn (struct board* b){
+void begin_turn (struct board* b){
 	b->n_turn+=1;
-	if is_peer(*b){
+	if (is_peer(*b)) {
 		b->player1.current_students.FISA_count=0;
 		b->player2.current_students.FISA_count=0;
 		b->player1.current_students.FISA_durability=0;
@@ -39,7 +39,7 @@ int is_over(struct board b){
 }
 
 int is_peer(struct board b){
-	if((b->n_turn)%2==0){
+	if ((b.n_turn)%2==0)
 		return 1;
 	else
 		return 0;
@@ -48,14 +48,15 @@ int is_peer(struct board b){
 void add_student_FISE(int a, struct ensiie* p){
 	p->current_students.FISE_count+=a;
 	p->current_students.FISE_development+=a;
-	p->current_students.FISE_durability+=a
+	p->current_students.FISE_durability+=a;
 }
 
 void add_student_FISA(int a, struct ensiie* p){
 	p->current_students.FISA_count+=a;
 	p->current_students.FISA_development+=a;
-	p->current_students.FISA_durability+=a
+	p->current_students.FISA_durability+=a;
 }
- void draw(ensiie* a){
 
- }
+void draw(struct ensiie* a){
+
+}
